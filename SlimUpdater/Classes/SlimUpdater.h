eb9641a4e192b9f27ba3dc77acd1f51cddd21aee
Rob Skillington <rskillington@groupon.com>
2014-01-17 17:00:16 -0800

    Adding static library template

---
+++ SlimUpdater/Classes/SlimUpdater.h
@@ -0,0 +1,13 @@
+//
+//  SlimUpdater.h
+//  SlimUpdater
+//
+//  Created by Rob Skillington on 1/17/14.
+//  Copyright (c) 2014 Rob Skillington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SlimUpdater : NSObject
+
+@end
---
+++ SlimUpdater/Classes/SlimUpdater.m
@@ -0,0 +1,13 @@
+//
+//  SlimUpdater.m
+//  SlimUpdater
+//
+//  Created by Rob Skillington on 1/17/14.
+//  Copyright (c) 2014 Rob Skillington. All rights reserved.
+//
+
+#import "SlimUpdater.h"
+
+@implementation SlimUpdater
+
+@end
